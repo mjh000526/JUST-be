48c8715e8572310b8b241d8198afc4a15af12b02
문준호 <munjunho@munjunhoui-MacBookAir.local>
2024-03-22 21:42:19 +0900

    notification 알림

---
+++ src/main/java/com/example/just/Controller/NotificationController.java
@@ -0,0 +1,61 @@
+package com.example.just.Controller;
+
+import com.example.just.Service.NotificationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class NotificationController {
+    @Autowired
+    private NotificationService notificationService;
+
+    @Operation(summary = "알림 조회", description = "헤더에 있는 값으로 판별\n")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "{\n"
+                    + "  \"not_id\": \"\",\n"
+                    + "  \"not_type\": 0\n"
+                    + "  \"not_datetime\": \"\",\n"
+                    + "  \"not_is_read\": 0\n"
+                    + "  \"receiver_id\": 0\n"
+                    + "  \"sender_id\": true\n"
+                    + "}"),
+            @ApiResponse(responseCode = "404", description = "{\n"
+                    + "  \"message\": \"페이지를 초과하였습니다.\"\n"
+                    + "}")
+    })
+    @GetMapping("/notifications")
+    public ResponseEntity getNotificationList(HttpServletRequest request,int page){
+        return notificationService.getNotificationList(request,page-1);
+    }
+
+    @Operation(summary = "알림 읽음 확인")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "{\n"
+                    + "  \"not_id\": \"\",\n"
+                    + "  \"not_type\": 0\n"
+                    + "  \"not_datetime\": \"\",\n"
+                    + "  \"not_is_read\": 0\n"
+                    + "  \"receiver_id\": 0\n"
+                    + "  \"sender_id\": true\n"
+                    + "}"),
+            @ApiResponse(responseCode = "404", description = "{\n"
+                    + "  \"message\": \"사용자의 알림이 아닙니다.\"\n"
+                    + "}")
+    })
+    @PutMapping("/check/notifications")
+    public ResponseEntity getNotificationList(HttpServletRequest request,@RequestParam Long not_id){
+        return notificationService.checkNotification(request,not_id);
+    }
+}
---
+++ src/main/java/com/example/just/Service/NotificationService.java
@@ -0,0 +1,65 @@
+package com.example.just.Service;
+
+import com.example.just.Dao.Notification;
+import com.example.just.Dto.Message;
+import com.example.just.Repository.MemberRepository;
+import com.example.just.Repository.NotificationRepository;
+import com.example.just.Response.ResponseMessage;
+import com.example.just.Response.ResponseNotification;
+import com.example.just.Response.ResponseSearchDto;
+import com.example.just.jwt.JwtProvider;
+import com.google.api.Http;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private NotificationRepository notificationRepository;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public ResponseEntity getNotificationList(HttpServletRequest request,int page){
+        String token = jwtProvider.getAccessToken(request);
+        Long member_id = Long.valueOf(jwtProvider.getIdFromToken(token)); //토큰
+        List<Notification> notifications = notificationRepository.findAllByReceiver(memberRepository.findById(member_id).get());
+        List<ResponseNotification> result = notifications.stream()
+                .map(noti -> new ResponseNotification(noti))
+                .collect(Collectors.toList());
+
+        PageRequest pageRequest = PageRequest.of(page,10);
+        result.sort(Comparator.comparing(ResponseNotification::getNot_datetime).reversed());//최신순 조회
+        int start = (int) pageRequest.getOffset();
+        if (start >= result.size()) {
+            return new ResponseEntity(new ResponseMessage("페이지를 초과하엿습니다."),null, HttpStatus.BAD_REQUEST);
+        }
+        int end = Math.min((start + pageRequest.getPageSize()),result.size());
+        Page<ResponseNotification> postPage = new PageImpl<>(result.subList(start,end), pageRequest, result.size());
+        return ResponseEntity.ok(postPage);
+    }
+
+    public ResponseEntity checkNotification(HttpServletRequest request,Long noti_id){
+        String token = jwtProvider.getAccessToken(request);
+        Long member_id = Long.valueOf(jwtProvider.getIdFromToken(token));
+        Notification notification = notificationRepository.findById(noti_id).get();
+        if(notification.getReceiver().getId() != member_id){
+            return new ResponseEntity(new ResponseMessage("사용자의 알림이 아닙니다."),HttpStatus.BAD_REQUEST);
+        }
+        notification.setNotIsRead(true);
+        notificationRepository.save(notification);
+        return new ResponseEntity(new ResponseNotification(notification), HttpStatus.OK);
+    }
+}
